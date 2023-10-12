2069f346ae10ee5895ee9c717fb861f46c653a8a
Brandon Sprague <brandon@sprague.mx>
2023-10-12 14:45:50 -0700

    Assorted tweaks, combine into a single image

---
+++ combined/install_dependencies.R
@@ -0,0 +1,14 @@
+dependencies <- c(
+  "bookdown",
+  "jsonlite",
+  "logger",
+  "rmarkdown",
+  "uuid"
+)
+
+install.packages(
+  pkgs = dependencies,
+  repos = "https://packagemanager.posit.co/cran/__linux__/jammy/2023-08-31",
+  dependencies = c("Depends", "Imports", "LinkingTo")
+)
+
